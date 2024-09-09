5dd481b3e2efe30503ae93ba9c06d3a466475a46
mattmc3 <mattmc3@gmail.com>
2024-09-09 13:17:59 -0400

    Update fisher event handlers

--- conf.d/omz-compat.fish
+++ conf.d/omz-compat.fish
@@ -1,12 +1,16 @@
+if not type -q omf2 && type -q fisher
+    fisher install oh-my-fish-2/omf2
+end
+
 # Support Fisher's plugin events
 function _omz_compat_install --on-event omz_compat_install
-    type --query omf2 && omf2 install oh-my-fish2/omz-compat
+    omf2 handle install oh-my-fish2/omz-compat
 end
 
 function _omz_compat_update --on-event omz_compat_update
-    type --query omf2 && omf2 update oh-my-fish2/omz-compat
+    omf2 handle update oh-my-fish2/omz-compat
 end
 
 function _omz_compat_uninstall --on-event omz_compat_uninstall
-    type --query omf2 && omf2 uninstall oh-my-fish2/omz-compat
+    omf2 handle uninstall oh-my-fish2/omz-compat
 end
