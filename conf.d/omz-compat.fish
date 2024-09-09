528302c5a6fccf1982e06b3911cddb78c578a4f9
mattmc3 <mattmc3@gmail.com>
2024-09-09 13:36:13 -0400

    Update conf.d

--- conf.d/omz-compat.fish
+++ conf.d/omz-compat.fish
@@ -4,13 +4,13 @@ end
 
 # Support Fisher's plugin events
 function _omz_compat_install --on-event omz_compat_install
-    omf2 handle install oh-my-fish2/omz-compat
+    omf2 handle install oh-my-fish-2/omz-compat
 end
 
 function _omz_compat_update --on-event omz_compat_update
-    omf2 handle update oh-my-fish2/omz-compat
+    omf2 handle update oh-my-fish-2/omz-compat
 end
 
 function _omz_compat_uninstall --on-event omz_compat_uninstall
-    omf2 handle uninstall oh-my-fish2/omz-compat
+    omf2 handle uninstall oh-my-fish-2/omz-compat
 end
